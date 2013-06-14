1664e45deb206551b5fea9717c51f57aa5232b62
Apprentice <apprentice@dbc07.local>
2013-06-14 15:35:24 -0700

    Modified the wrong limit to keep the game going in FlashCards

--- FlashCards.rb
+++ FlashCards.rb
@@ -12,32 +12,30 @@ class FlashCards
     @deck = @parser.read_in_cards
     @current_card = nil
     @input = ""
+    @wrong_limit = 0
     game_start
     play_game
   end
 
   def play_game
     play_card
-    wrong_limit = 0
-    while wrong_limit < 3
+    while true
       if @current_card.is_answer?(@input)
         correct_answer
-        wrong_limit = 0
         play_card
       elsif @input == "answer"
         gave_up
         @current_card.print_answer
         play_card
-        wrong_limit = 0
       elsif @input == "fuck you"
         naughty_words
         @input = gets.chomp
       elsif @input == "exit"
         break
       else
-        wrong_limit += 1
         wrong_answer
-        @input = gets.chomp
+        @wrong_limit += 1
+        @wrong_limit >= 3 ? play_card : @input = gets.chomp
       end
     end
   end
@@ -46,6 +44,7 @@ class FlashCards
     @current_card = @deck.get_card
     @current_card.print_definition
     @input = gets.chomp
+    @wrong_limit = 0
   end
 
 

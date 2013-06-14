1a0bd9dded24dc1f12c3c11df0bb6bfe35b0cfb6
Apprentice <apprentice@dbc07.local>
2013-06-14 15:29:13 -0700

    Implemented wrong answer count, exits after too many stupid guesses

--- FlashCards.rb
+++ FlashCards.rb
@@ -18,20 +18,24 @@ class FlashCards
 
   def play_game
     play_card
-    while true
+    wrong_limit = 0
+    while wrong_limit < 3
       if @current_card.is_answer?(@input)
         correct_answer
+        wrong_limit = 0
         play_card
       elsif @input == "answer"
         gave_up
         @current_card.print_answer
         play_card
+        wrong_limit = 0
       elsif @input == "fuck you"
         naughty_words
         @input = gets.chomp
       elsif @input == "exit"
         break
       else
+        wrong_limit += 1
         wrong_answer
         @input = gets.chomp
       end

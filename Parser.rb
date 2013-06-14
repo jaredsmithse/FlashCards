2ea3ecf5ee88bed98ad35084e86552e06411d775
Jared Smith <jared.rit@me.com>
2013-06-14 12:09:38 -0700

    Added make_cards and make_deck to Parser

--- Parser.rb
+++ Parser.rb
@@ -15,11 +15,21 @@ class Parser
     flashcard_array.delete_if { |element| element == ""}
     output = []
     flashcard_array.each_slice(2) { |card| output << card }
-    p output.inspect
+    puts make_deck(make_cards(output))
   end
 
+  private
+  def make_cards(card_info)
+    @card_array = []
+    card_info.each do |card|
+      @card_array << Card.new(card[0],card[1])
+    end
+    @card_array
+  end
 
-
+  def make_deck(cards)
+    Deck.new(cards)
+  end
 end
 
 text = Parser.new('./flashcard_samples.txt')
